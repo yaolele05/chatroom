8bf4c8294e58a91e93c43b683283b057f593a8ab
yaolele05 <3256408162@qq.com>
2026-06-23 20:19:20 +0800

    epllpoller.h

---
+++ minimuduo/net/epollpoller.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "poller.h"
+#include <vector>
+#include <sys/epoll.h>
+
+class Channel;
+
+class EpollPoller : public Poller
+{
+public:
+    EpollPoller();
+    ~EpollPoller() override;
+
+    EpollPoller(const EpollPoller&) = delete;
+    EpollPoller& operator=(const EpollPoller&) = delete;
+
+    void poll(int timeoutMs,
+              std::vector<Channel*>& activeChannels) override;
+
+    void updateChannel(Channel* channel) override;
+    void removeChannel(Channel* channel) override;
+
+private:
+    void fillActiveChannels(int numEvents,
+                            std::vector<Channel*>& activeChannels);
+
+private:
+    int epfd_;   
+
+    std::vector<epoll_event> events_;
+};
