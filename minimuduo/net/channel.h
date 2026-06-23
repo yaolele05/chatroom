9bb59e8de0f5518cfd7782e7ec9b1814ecb3f597
yaolele05 <3256408162@qq.com>
2026-06-23 20:16:23 +0800

    channel.h

--- minimuduo/net/channel.h
+++ minimuduo/net/channel.h
@@ -1,23 +1,76 @@
 #pragma once
+
 #include <functional>
+#include <cstdint>
+
 class EventLoop;
+
 class Channel
 {
-    public:
-    Channel(EventLoop*loop,int fd_);
+public:
+    using EventCallback = std::function<void()>;
 
-    int fd() const{return fd;};
+    Channel(EventLoop* loop,int fd);
+    ~Channel() = default;
+
+    void handleEvent();
+
     
-    void setreadcallback(std::function<void()>cb);
-    void setwritecallback(std::function<void()>cb);
-    void setclosecallback(std::function<void()>cb);
+    void setReadCallback(EventCallback cb);
+    void setWriteCallback(EventCallback cb);
+    void setCloseCallback(EventCallback cb);
 
-    void handleevent(int events);
-    void enableread();
-    private:
-    EventLoop*loop;
-    int fd;
-    std::function<void()>readcb;
-    std::function<void()>writecb;
-    std::function<void()>closecb;
-}
+    // fd
+    int fd() const
+    {
+        return fd_;
+    }
+
+    // epoll events
+    uint32_t events() const
+    {
+        return events_;
+    }
+
+    uint32_t revents() const
+    {
+        return revents_;
+    }
+
+    void setRevents(uint32_t ev)
+    {
+        revents_ = ev;
+    }
+
+    // 状态控制
+    bool inEpoll() const
+    {
+        return inEpoll_;
+    }
+
+    void setInEpoll(bool flag)
+    {
+        inEpoll_= flag;
+    }
+
+    // 用户接口
+    void enableReading();
+    void enableWriting();
+
+    void disableWriting();
+    void disableAll();
+
+private:
+    EventLoop* loop_;
+
+    int fd_;
+
+    uint32_t events_;
+    uint32_t revents_;
+
+    bool inEpoll_;
+
+    EventCallback readCallback_;
+    EventCallback writeCallback_;
+    EventCallback closeCallback_;
+};
