c8c059a2287a56ce68f40b54026798fdda2cea91
yaolele05 <3256408162@qq.com>
2026-06-23 20:18:27 +0800

    poll

--- minimuduo/net/poller.h
+++ minimuduo/net/poller.h
@@ -1,11 +1,24 @@
-#pragma once 
+#pragma once
+
 #include <vector>
+
 class Channel;
+
 class Poller
 {
-    public:
-    virtual ~Poller()=default;
-    virtual void poll(int timeoutms,std::vector<Channel*>activeChannels)=0;
-    virtual void upateChannel(Channel* channel)=0;
-    virtual void removeChannel(Channel* channel)=0;
+public:
+    Poller() = default;
+    virtual ~Poller() = default;
+
+    Poller(const Poller&) = delete;
+    Poller& operator=(const Poller&) = delete;
+
+    // 等待事件发生（epoll_wait 封装）
+    virtual void poll(int timeoutMs,std::vector<Channel*>& activeChannels) = 0;
+
+    // 更新 Channel（add / mod）
+    virtual void updateChannel(Channel* channel) = 0;
+
+    // 删除 Channel
+    virtual void removeChannel(Channel* channel) = 0;
 };
