940a6f9884d045dc691d8a55d2275dd5d647006d
yaolele05 <3256408162@qq.com>
2026-07-20 17:35:44 +0800

    channel

--- minimuduo/net/channel.cpp
+++ minimuduo/net/channel.cpp
@@ -2,15 +2,26 @@
 #include "eventloop.h"
 #include <sys/epoll.h>
 Channel::Channel(EventLoop* loop,int fd): loop_(loop),fd_(fd),events_(0),revents_(0),inEpoll_(false){}
+void Channel::setReadCallback(EventCallback cb)
+{
+     readCallback_=std::move(cb);
+}
+void Channel::setWriteCallback(EventCallback cb)
+{
+    writeCallback_=std::move(cb);
+}
+void Channel::setCloseCallback(EventCallback cb)
+{
+    closeCallback_=std::move(cb);
+}
 void Channel::handleEvent()
 {
-    if(revents_&(EPOLLERR|EPOLLHUP))
+    if(revents_ & EPOLLHUP)
     {
         if(closeCallback_)
         {
             closeCallback_();
         }
-        return;
     }
     if(revents_&EPOLLIN)
     {
@@ -26,4 +37,34 @@ void Channel::handleEvent()
             writeCallback_();
         }
     }
+
+}
+void Channel::enableReading()
+{
+    events_ |= EPOLLIN;
+    loop_->updateChannel(this);
+}
+void Channel::enableWriting()
+{
+    events_ |= EPOLLOUT;
+    loop_->updateChannel(this);
+}
+void Channel::disableReading()
+{
+    events_ &= ~EPOLLIN;
+    loop_->updateChannel(this);
+}
+void Channel::disableWriting()
+{
+    events_ &= ~EPOLLOUT;
+    loop_->updateChannel(this);
+}
+void Channel::disableAll()
+{
+    events_=0;
+    loop_ ->updateChannel(this);
+}
+void Channel::remove()
+{
+    loop_->removeChannel(this);
 }
