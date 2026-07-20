e419831f89e2992168718f0315ea8e8b84c1516b
yaolele05 <3256408162@qq.com>
2026-07-20 17:36:29 +0800

    poller

--- minimuduo/net/poller.h
+++ minimuduo/net/poller.h
@@ -12,9 +12,10 @@ public:
 
     Poller(const Poller&) = delete;
     Poller& operator=(const Poller&) = delete;
-
+    
+   using ChannelList = std::vector<Channel*>;
     // 等待事件发生（epoll_wait 封装）
-    virtual void poll(int timeoutMs,std::vector<Channel*>& activeChannels) = 0;
+    virtual void poll(int timeoutMs,ChannelList& activeChannels) = 0;
 
     // 更新 Channel（add / mod）
     virtual void updateChannel(Channel* channel) = 0;
