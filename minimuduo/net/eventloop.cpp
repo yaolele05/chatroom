ec55df02525ca59a281cf354332621eb549b769e
yaolele05 <3256408162@qq.com>
2026-07-21 21:34:32 +0800

    eventloop

--- minimuduo/net/eventloop.cpp
+++ minimuduo/net/eventloop.cpp
@@ -1,26 +1,51 @@
 #include "eventloop.h"
 #include "channel.h"
 #include "epollpoller.h"
+#include <unistd.h>
+#include <cassert>////
+#include <atomic>
+#include <sys/eventfd.h>
+#include <cstdio>
+static int createEventfd()
+{
+    int fd=eventfd(0,EFD_NONBLOCK | EFD_CLOEXEC);
+    if(fd<0)
+    {
+        abort();
 
-#include<cassert>////
-EventLoop::EventLoop():looping_(false),quit_(false),threadId_(std::this_thread::get_id()),poller_(new EpollPoller()){}
+    }
+    return fd;
+}
+EventLoop::EventLoop():looping_(false),wakeupfd_(createEventfd()),quit_(false),callingPendingFunctors_(false),threadId_(std::this_thread::get_id()),poller_(new EpollPoller()),wakeupChannel_(new Channel(this,wakeupfd_)){
+
+
+    wakeupChannel_->setReadCallback(std::bind(&EventLoop::handleRead,this));
+    wakeupChannel_->enableReading();
+}
+EventLoop::~EventLoop()
+{
+    close(wakeupfd_);
+}
 void EventLoop::loop()
 {
     looping_=true;
     while(!quit_)
     {
     activeChannels_.clear();
-    poller_->poll(1000,activeChannels_);
+    poller_->poll(10000,activeChannels_);
     for(Channel* channel:activeChannels_)
     {
         channel->handleEvent();
     }
+
+    doPendingFunctors();
     }
     looping_=false;
 }
 void EventLoop::quit()
 {
-    quit_=true;
+    
+    std::atomic<bool>quit_;
 }
 
 void EventLoop::updateChannel(Channel* channel)
@@ -35,4 +60,62 @@ void EventLoop::removeChannel(Channel*channel)
 }
 bool EventLoop::isInLoopThread()const{
     return threadId_==std::this_thread::get_id();
+}
+void EventLoop::runInLoop(Functor cb)
+{
+    if(isInLoopThread())
+    {
+        cb();
+    }
+    else
+    {
+        queueInLoop(cb);
+    }
+}
+void EventLoop::queueInLoop(Functor cb)
+{
+    {
+        std::lock_guard<std::mutex>lock(mutex_);
+        pendingFunctors_.push_back(std::move(cb));
+    }
+    if(!isInLoopThread()||callingPendingFunctors_)
+    {
+    wakeup();
+    }
+}
+void EventLoop::wakeup()
+{
+    uint64_t one=1;
+
+    ssize_t n=write(wakeupfd_,&one,sizeof(one));
+
+    if(n!=sizeof(one))
+    {
+       if(errno!=EAGAIN)
+       {
+        perror("eventfd write");
+       }
+    }
+}
+void EventLoop::handleRead()
+{
+    uint64_t one;
+    ssize_t n=::read(wakeupfd_,&one,sizeof(one));
+    if(n!=sizeof(one))
+    {
+        perror("eventfd read");
+    }
+}
+void EventLoop::doPendingFunctors()
+{
+    std::vector<Functor>functors;
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        functors.swap(pendingFunctors_);
+    }
+
+    for(auto& functor:functors)
+    {
+        functor();
+    }
 }
