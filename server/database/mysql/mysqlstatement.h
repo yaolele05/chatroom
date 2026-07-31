78901c85f27e02c101dc4df43ed0aa40fb454d25
yaolele05 <3256408162@qq.com>
2026-07-31 21:22:51 +0800

    mysqlstatement

---
+++ server/database/mysql/mysqlstatement.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <mysql/mysql.h>
+#include "mysqlresult.h"
+#include <string>
+#include <vector>
+#include<cstdint>
+#include <variant>
+class MysqlStatement
+{
+    public:
+    explicit MysqlStatement(MYSQL* mysql_);
+   ~MysqlStatement();
+      MysqlStatement(const MysqlStatement&) = delete;
+    MysqlStatement& operator=(const MysqlStatement&) = delete;
+    bool prepare (const std::string_view sql);
+    bool bind(int index,const std::string& value );
+    bool bind(int index,int value);
+    bool bind(int index,bool value);
+    bool bind(int index,uint64_t value);
+    bool bind(int index,double value);
+    
+    bool execute();
+  
+    MysqlResult query();
+    
+   private:
+    MYSQL_STMT* stmt_;
+    std::vector<MYSQL_BIND> binds_;
+    using BindValue =std::variant<std::monostate,std::string,int32_t,bool,uint64_t,double>;
+
+    std::vector<BindValue> values_;
+
+
+};
