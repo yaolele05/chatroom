325913093ea70cf95eab96f53f71671d4b4b216b
yaolele05 <3256408162@qq.com>
2026-07-31 21:21:32 +0800

    mysqlclient

--- server/database/mysql/mysqlclient.cpp
+++ server/database/mysql/mysqlclient.cpp
@@ -22,10 +22,12 @@ bool MysqlClient::connect(const std::string& host,uint16_t port,const std::strin
     if(mysql_==nullptr)
     return false;
     MYSQL*ret=mysql_real_connect(mysql_,host.c_str(),username.c_str(),password.c_str(),database.c_str(),port,nullptr,0);
+   
     if(ret==nullptr)
     {
-        std::cerr<<"mysql connect failed"<<std::endl;
-        return false;
+    std::cerr << "mysql connect failed: "<< mysql_error(mysql_)<< std::endl;
+
+    return false;
     }
      mysql_set_character_set(mysql_,"utf8mb4");
      return true;
@@ -48,29 +50,12 @@ bool MysqlClient::execute(std::string_view sql)///
         if(mysql_==nullptr)
         return false;
 
-        if(mysql_query(mysql_,sql.data())!=0)
+        if(mysql_real_query(mysql_,sql.data(),static_cast<unsigned long> (sql.size()))!=0)
         {
             std::cerr<<mysql_error(mysql_)<<std::endl;
             return false;
         }
    return true;
-}
-std::shared_ptr <MysqlResult> MysqlClient::query(std::string_view sql)
-{
-  if(mysql_==nullptr)
-   return nullptr;
-
-   if(mysql_query(mysql_,sql.data())!=0)
-   {
-    std::cerr<<mysql_error(mysql_)<<std::endl;
-    return nullptr;
-   }
-    
-   MYSQL_RES* res=mysql_store_result(mysql_);
-   if(res==nullptr)
-   return nullptr;
-
-   return std::make_unique<MysqlResult>(res);
 }
 bool MysqlClient::beginTransaction()
 {
@@ -91,4 +76,34 @@ std::string MysqlClient::error() const
         return mysql_error(mysql_);
     }
     return "mysql_ is null";
+}
+bool MysqlClient::ping()
+{
+    if (!mysql_)
+    {
+        return false;
+    }
+
+    return mysql_ping(mysql_) == 0;
+}
+std::unique_ptr<MysqlStatement> MysqlClient::prepare(std::string_view sql)
+{
+    if(!mysql_)
+    {
+        return nullptr;
+    }
+    auto stmt=std::make_unique<MysqlStatement>(mysql_);
+    if(!stmt->prepare(sql))
+    return nullptr;
+
+    return stmt;
+}
+uint64_t MysqlClient::lastInsertId() const
+{
+    if(!mysql_)
+    {
+        return 0;
+    }
+
+    return mysql_insert_id(mysql_);
 }
