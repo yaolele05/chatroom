42c1d30a38a3fa0d2edfafa0f588035f755eca18
yaolele05 <3256408162@qq.com>
2026-07-31 21:33:39 +0800

    redisclient

--- server/database/redis/redisclient.h
+++ server/database/redis/redisclient.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include<cstdarg>
 
 class RedisResult;
 
@@ -23,26 +24,34 @@ public:
 public:
 
     bool connect(const std::string& host, uint16_t port);
-
     void disconnect();
-
     bool connected() const;
 
-   
-
     bool set(const std::string& key,const std::string& value);
-
     bool setex(const std::string& key,int seconds,const std::string& value);
-
     std::optional<std::string> get(const std::string& key);
-
     bool del(const std::string& key);
-
     bool exists(const std::string& key);
+    bool expire(const std::string& key,int seconds); 
 
-    bool expire(const std::string& key,int seconds);
+     bool subscribe(const std::string& channel);
+     bool unsubscribe(const std::string& channel);
 
-    
+     bool setUserOnline(int userid);
+     bool setUserOffline(int userid);
+    bool isUserOnline(int userid);
+
+    bool saveToken(int userid, const std::string& token);
+    std::optional<std::string> getToken(int userid);
+     
+    RedisResult command(const std::string& cmd);
+    RedisResult command(const char* com,...);
+    //RedisResult commandBinary(const char* com,...);
+
+
+    bool publish(const std::string& channel,const std::string& message);
+
+    bool refreshHeartbeat(int userid);
 private:
 
     redisContext* context_{nullptr};
