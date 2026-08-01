e292676a665fe814e8bc518f4290064feb4e9c71
yaolele05 <3256408162@qq.com>
2026-08-01 17:50:57 +0800

    messagemodel

--- server/model/messagemodel.h
+++ server/model/messagemodel.h
@@ -1,13 +1,20 @@
 #pragma once
 #include <vector>
 #include "entity/message.h"
+#include <mysql/mysql.h>
+#include <optional>
+#include "../database/mysql/mysqlresult.h"
 class MessageModel
 {
    public:
-   bool insert(const Message& message);
-   std::vector<Message> queryPriHistory(int userid, int peerid);
-   std::vector<Message> queryGroupHistory(int userid,int groupid);
-   bool removePriHistory(int userid,int peerid);
-   bool removeGroupHistory(int groupid);
+   bool insert(Message& message);
+   std::optional<Message> findById(std::int64_t id);
+   std::vector<Message> findPriHistory(int userid, int peerid,size_t limit,size_t offset);
+   std::vector<Message> findGroupHistory(int groupid,size_t limit,size_t offset);
+   std::vector<Message> findUserGroupHistory(int userid, int groupid, size_t limit, size_t offset);
+
+
+   private:
+   Message makeMessage(MysqlResult& result);
 
 };
