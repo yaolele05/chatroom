1f726a136fd2f0609733f5249ef8823ad1a18d26
yaolele05 <3256408162@qq.com>
2026-08-03 21:34:01 +0800

    offlinemodel

--- server/model/offlinemodel.h
+++ server/model/offlinemodel.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <vector>
-#include"entity/message.h"
+#include "entity/offlinemessage.h"
+#include <cstdint>
+#include "../database/mysql/mysqlresult.h"
 class OfflineMessageModel
 {
     public:
-    bool insert(const Message& message);
+    bool insert(const OfflineMessage& message);
 
-    std::vector<Message> query(int userid);
-    bool remove(int userid);
-    bool clear(int userid);
+    std::vector<OfflineMessage> findByUserId(int userid);
+    bool remove(std::int64_t id);
+    bool clearUserMessages(int userid);
 
+    private:
+    OfflineMessage makeOfflineMessage(const MysqlResult& result);
 };
