3fb8c682fbb8e5849c899e0ea242e86ce9399f40
yaolele05 <3256408162@qq.com>
2026-08-03 21:31:52 +0800

    friendmodel

--- server/model/friendmodel.h
+++ server/model/friendmodel.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <vector>
 #include "entity/friend.h"
+#include "../database/mysql/mysqlresult.h"
 class FriendModel
 {
     public:
-    bool insert(const Friend& rela);
+    bool addFriend( Friend& rela);
     bool removeFriend(int userid,int friendid);
     bool isFriend(int userid,int friendid);
+    
+    std::vector<Friend> findFriends(int userid);
 
-    std::vector<Friend> queryFriendIds(int userid);
+    private:
+    Friend makeFriend(MysqlResult& result);
+    
 };
