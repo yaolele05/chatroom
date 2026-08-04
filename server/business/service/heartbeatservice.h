f71eabc17df10658c94902613a3f5b47fe9f01c7
yaolele05 <3256408162@qq.com>
2026-08-04 21:50:58 +0800

    heartbeatservice

---
+++ server/business/service/heartbeatservice.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"../../session/session.h"
+#include <nlohmann/json.hpp>
+#include "../../protocol/message.h"
+class HeartbeatService
+{
+    public:
+    static HeartbeatService& instance();
+    static void registerHandler();
+    void heartbeat(const Message& message,Session* session);
+    
+         
+
+    private:
+    HeartbeatService()=default;
+    void updateSession(Session*session);
+    void updateRedis(Session* session);
+
+};
