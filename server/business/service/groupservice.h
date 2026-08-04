c51b66ef172ee12e218679ec41ecb269763621b0
yaolele05 <3256408162@qq.com>
2026-08-04 21:49:01 +0800

    groupservice

---
+++ server/business/service/groupservice.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../protocol/message.h"
+#include "../../session/session.h"
+class GroupService
+{
+    public:
+    
+    static GroupService& instance();
+    static void rigisterHandler();
+    void createGroup(const Message& msg, Session* se);
+    void joinGroup(const Message& msg,Session* se);
+    void leaveGroup(const Message& msg,Session* se);
+    void groupChat(const Message& msg,Session* se);
+    private:
+    GroupService()=default;
+};
