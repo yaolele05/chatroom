7389d18f8dd847c48a2a0c7727be7a2d17f1402b
yaolele05 <3256408162@qq.com>
2026-08-06 21:30:30 +0800

    client

---
+++ client/clientconnection.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "../common/buffer.h"
+#include "../common/protocol/message.h"
+#include "../minimuduo/net/channel.h"
+#include "../minimuduo/net/socket.h"
+#include <functional>
+#include <memory>
+#include <string>
+class EventLoop;
+class ClientConnection:public std::enable_shared_from_this<ClientConnection>
+{
+    public:
+
+    using MessageCallback=std::function<void(const Message&)>;
+    ClientConnection(EventLoop* loop,int sockfd);
+
+    ~ClientConnection();
+    void send(const Message& message);
+    void close();
+    void setMessageCallback(MessageCallback cb);
+    void connectEstablished();
+    void connectDestroyed();
+    private:
+    void handleRead();
+     void handleWrite();
+     void handleClose();
+     void handleError();
+    void sendInLoop(const std::string& data);
+
+    private:
+    EventLoop* loop_;
+     Socket socket_;
+    Channel channel_;
+    std::unique_ptr<Buffer> inputBuffer_;
+    std::unique_ptr<Buffer> outputBuffer_;
+    MessageCallback messageCallback_;
+
+};
