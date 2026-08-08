97397141afbec84f0b8ba47aae5cc4bd0de1e59a
yaolele05 <3256408162@qq.com>
2026-08-08 23:10:43 +0800

    client

--- client/client.h
+++ client/client.h
@@ -1,27 +1,67 @@
 #pragma once
-#include <string>
+#include <condition_variable>
+#include <mutex>
 #include "tcpclient.h"
 #include "../common/protocol/message.h"
 #include <memory>
+#include <string>
+#include "clientconnection.h"
 
 class EventLoop;
-class ClientConnection;
+class TcpClient;
+class FileTransfer;
+
 class Client
 {
     public:
-    Client(EventLoop* loop);
+    explicit Client(EventLoop* loop);
+     ~Client();
 
-    bool connect(const std::string& ip,uint16_t port);
-   void login(const std::string& username,const std::string& password);
-   void registerUser(const std::string& username,const std::string& password);
-   void sendPrivateMessage(int userid,const std::string& text);
-   void sendGroupMessage(int64_t groupid,const std::string& text);
-
-
+    bool connect(const std::string&ip,uint16_t port);
+    void login(const std::string& username,const std::string& password);
+    void logout();
+    void addFriend(uint32_t friendId);
+    void deleteFriend(uint32_t friendId);
+     void friendList();
+    void privateChat(uint32_t userId,const std::string& text);
+    void createGroup(const std::string& groupName,const std::string& description);
+    void joinGroup(uint32_t groupId);
+    void leaveGroup(uint32_t groupId);
+    void groupChat(uint32_t groupId,const std::string& text);
+     void groupList();
+    void sendFile(uint32_t userId,const std::string& filename);
+    void sendImage(uint32_t userId,const std::string& filename);
+    bool isLogin() const;
+    void registerUser(const std::string& username,const std::string& password);
+   
+     void disconnect();
+     void quit();
+     bool waitLoginResult();
     private:
    void onMessage(const Message& message);
+   void handleLogin(const Message& msg);
+   void handleLogout(const Message& msg);
+   void handlePrivateChat(const Message& msg);
+   void handleFriend(const Message& msg);
+   void handleGroup(const Message& msg);
+   void handleGroupChat(const Message& msg);
+   void handleGroupList(const Message& msg);
+   void handleFile(const Message& msg);
+   void handleHeartbeat(const Message& msg);
+    void handleRegister(const Message& msg);
+ 
+   
    private:
    EventLoop* loop_;
    std::unique_ptr<TcpClient> tcpClient_;
-   std::unique_ptr<ClientConnection> clientConn_;
+   std::unique_ptr<FileTransfer> fileTransfer_;
+   std::shared_ptr<ClientConnection> connection_;
+
+    std::string username_;
+   uint32_t userId_{0};
+    uint64_t sequence_{1};
+   bool login_{false};
+   bool loginFinished_{false};
+   std::mutex loginMutex_;
+std::condition_variable loginCv_;
 };
