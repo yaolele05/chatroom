ad852028c6d1eaecfb37b679da06ac3767373b63
yaolele05 <3256408162@qq.com>
2026-08-08 23:12:36 +0800

    filetransf

---
+++ client/filetransf.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <memory>
+#include <fstream>
+#include <atomic>
+#include "clientconnection.h"
+class FileTransfer
+{
+    public:
+    struct SendTask
+   {
+    uint64_t fileId{0};
+    uint64_t serverFileId;//服务器数据库fileid
+    uint32_t receiverId{0};
+    std::string filename;
+    uint64_t filesize{0};
+      std::string sha256;  
+    uint64_t offset{0};
+    std::ifstream file;
+    bool finished=false;
+   };
+    struct ReceiveTask
+   {
+    std::ofstream file{0};
+    std::string filename;
+    uint64_t filesize{0};
+    uint64_t received{0};
+    std::string sha256;
+   std::string filepath;
+   };
+   struct PendingFile
+  {
+    uint32_t receiverId{0};
+    std::string filename;
+    uint64_t filesize{0};
+    std::string sha256;
+  };
+    explicit FileTransfer(std::shared_ptr<ClientConnection>conn);
+    bool sendFile(uint32_t receiverId,const std::string& filename);
+    void sendChunks(uint64_t fileId,uint64_t offset);
+    bool sendImage(uint32_t receiverId,const std::string& filename);
+
+    void handleFileStart(const Message& msg);
+    void handleFileChunk(const Message& msg);
+    void handleFileFinish(const Message& msg);
+    void handleAck(const Message& msg);
+    void handleResumeRequest(const Message&msg);
+    void handleResumeResponse(const Message&msg);
+
+    
+    private:
+     
+    std::shared_ptr <ClientConnection> connection_; 
+    std::string saveDirectory_{"download/"};
+    std::unordered_map<uint64_t,SendTask> sendTasks_;
+    std::unordered_map<uint64_t,ReceiveTask> receiveTasks_;
+    PendingFile pendingFile_;
+    bool hasPendingFile_{false};
+};
