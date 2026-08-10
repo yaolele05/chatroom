ee6b224e9398820a619581abec2ec8d1196bd8a7
yaolele05 <3256408162@qq.com>
2026-08-10 19:05:16 +0800

    offlineservive

--- server/business/service/offlineservice.cpp
+++ server/business/service/offlineservice.cpp
@@ -5,11 +5,13 @@
 #include"../../model/messagemodel.h"
 #include <chrono>
 #include "../../model/filemodel.h"
+#include "../businessdispatcher/businessdispatcher.h"
 OfflineService& OfflineService::instance()
 {
     static OfflineService service;
     return service;
 }
+
 void OfflineService::sendOfflineMessage(Session* se)
 {
     if(se==nullptr)
@@ -27,18 +29,24 @@ void OfflineService::sendOfflineMessage(Session* se)
      std::cout << "offline count = " << messages.size()<< std::endl;
     for(auto& offline:messages)
     {
+        bool success=false;
         switch(offline.type())
         {
         case OfflineType::ChatMessage:
         sendChatOffline(offline,se);
+        success=true;
         break;
         case OfflineType::File:
         sendFileOffline(offline,se);
+        success=true;
         break;
         default:
         break;
         }
+        if(success)
+        {
         model.remove(offline.id());
+        }
     }
 }
 void::OfflineService::sendChatOffline(const OfflineMessage& offline,Session* se)
@@ -48,6 +56,11 @@ void::OfflineService::sendChatOffline(const OfflineMessage& offline,Session* se)
    if(!message)
    return;
   
+   if(message->groupId()!=0)
+   {
+    sendGroupOffline(offline,se);
+    return;
+   }
    Message reply;
    reply.setType(Messagetype::PrivateChat);
    reply.setSenderId(message->sendId());
@@ -59,13 +72,32 @@ void::OfflineService::sendChatOffline(const OfflineMessage& offline,Session* se)
 
    se->send(reply);
 }
-void OfflineService::sendFileOffline(const OfflineMessage& offline,Session* se)
+void OfflineService::sendGroupOffline(const OfflineMessage& offline,Session* se)
+{
+    MessageModel model;
+     auto message=model.findById(offline.messageId());
+   if(!message)
+   return;
+      Message reply;
+   reply.setType(Messagetype::GroupChat);
+   reply.setSenderId(message->sendId());
+  
+   auto timestamp=std::chrono::duration_cast<std::chrono::seconds>(message->sendTime().time_since_epoch()).count();
+   reply.setTimestamp(timestamp);
+   reply.payload()["groupId"]=message->groupId();
+   reply.payload()["content"]=message->content();
+
+   se->send(reply);
+
+}
+bool OfflineService::sendFileOffline(const OfflineMessage& offline,Session* se)
 {
     FileModel model;
     auto file=model.findById(offline.messageId());
     if(!file)
     {
-        return;
+        std::cout<<"find filebyID failed"<<std::endl;
+        return false;
     }
     Message msg;
     msg.setType(Messagetype::FileFinish);
@@ -80,4 +112,5 @@ void OfflineService::sendFileOffline(const OfflineMessage& offline,Session* se)
     msg.payload()["sha256"]=file->fileSha256();
 
     se->send(msg);
+    return true;
 }
