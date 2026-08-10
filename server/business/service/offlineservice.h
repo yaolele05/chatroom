e9c636768ff73c679438cfb9188e26fb3f58b919
yaolele05 <3256408162@qq.com>
2026-08-10 19:04:54 +0800

    offlineservive

--- server/business/service/offlineservice.h
+++ server/business/service/offlineservice.h
@@ -6,15 +6,16 @@ class OfflineService
     public:
     static OfflineService& instance();
     void sendOfflineMessage(Session* session);
-    
+  
     private:
     OfflineService()=default;
     OfflineService(const OfflineService&)=delete;
     OfflineService& operator=(const OfflineService&)=delete;
 
     void sendChatOffline(const OfflineMessage&offline,Session* se);
-    void sendFileOffline(const OfflineMessage&offline,Session* se);
-
+    void sendGroupOffline(const OfflineMessage& offline,Session* se);
+    bool sendFileOffline(const OfflineMessage&offline,Session* se);
+    
  
 
 };
